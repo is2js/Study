2a98a2ae01fd033a5a9f3f8b7de6ab9046909698
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-05-31 13:59:59 +0900

    [ActivityControl] 소스코드 추가

--- Android/Mini Project/ActivityControl/app/src/main/java/com/mdy/android/activitycontrol/MainActivity.java
+++ Android/Mini Project/ActivityControl/app/src/main/java/com/mdy/android/activitycontrol/MainActivity.java
@@ -5,37 +5,46 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button buttonStart, buttonForResult;
-    Intent intentForSubActivity;
+    Intent intent;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intentForSubActivity = new Intent(this, SubActivity.class);
+        intent = new Intent(this, SubActivity.class);
 
         buttonStart = (Button) findViewById(R.id.btnStart);
         buttonForResult = (Button) findViewById(R.id.btnForResult);
+        editText = (EditText) findViewById(R.id.editText);
 
         buttonStart.setOnClickListener(this);
         buttonForResult.setOnClickListener(this);
 
+
+
     }
+
+    public static final int BUTTON_RESULT = 99;
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             // 일반적인 Activity start
             case R.id.btnStart:
-                startActivity(intentForSubActivity);
+                startActivity(intent);
                 break;
             // 값을 돌려받는 Activity start
             case R.id.btnForResult:
-                startActivityForResult(intentForSubActivity, 100);
+                intent.putExtra("key", editText.getText().toString());  //  putExtra("변수", "값")
+
+                startActivityForResult(intent, BUTTON_RESULT);
                 // startActivityForResult 두번째 인자는 호출받는 곳에서 구별할 수 있게 해주는 구분자이다.
 
                 // > start SubActivity > finish() > 결과값을 돌려준다. > MainActivity.onActivityResult(결과값) 메소드를 시스템이 호출한다.
--- Android/Mini Project/ActivityControl/app/src/main/java/com/mdy/android/activitycontrol/SubActivity.java
+++ Android/Mini Project/ActivityControl/app/src/main/java/com/mdy/android/activitycontrol/SubActivity.java
@@ -1,14 +1,48 @@
 package com.mdy.android.activitycontrol;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        // 1. 이전 activity에서 넘어온 intent 객체
+        Intent intent = getIntent(); // 여기는 null이 안된다.
+        // 2. 값의 묶음을 꺼낸다. // getExtras()의 반환값은 bundle 형태이다.
+        Bundle bundle = intent.getExtras(); // 여기는 전달된 값이 없으면 null이 된다.
+        // 3. 단일 값을 꺼낸다. 꺼내기 전에 null 체크를 해줘야 한다.
+        if(bundle != null) {
+            String value = bundle.getString("key");     // .putExtra("변수", "값")으로
+            textView.setText(value);
+        }
+
+
+
+        // 이렇게 한번에 해줘도 된다.
+        // getIntent().getExtras().getString("key");
+
+        // 위의 두 줄(2,3번)을 합쳐놓은 method
+        // - 자체적으로 bundle에 대한 null 처리 로직을 포함하고 있다.
+        // String value = intent.getStringExtra("key");
+
+
 
     }
 }
