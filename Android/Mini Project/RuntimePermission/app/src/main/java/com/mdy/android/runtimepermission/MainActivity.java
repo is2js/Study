6ee9c98b8545895a3976dee10e01e2a3c3c415e4
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-05-31 16:13:31 +0900

    [RuntimePermission] 프로젝트 생성

---
+++ Android/Mini Project/RuntimePermission/app/src/main/java/com/mdy/android/runtimepermission/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mdy.android.runtimepermission;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        
+
+    }
+}
