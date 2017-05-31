e221627ba75ac05090b5232b9eb9acc168b4667c
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-05-31 12:26:41 +0900

    [ActivityControl] 프로젝트 생성

---
+++ Android/Mini Project/ActivityControl/app/src/main/java/com/mdy/android/activitycontrol/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mdy.android.activitycontrol;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button buttonStart, buttonForResult;
+    Intent intentForSubActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentForSubActivity = new Intent(this, SubActivity.class);
+
+        buttonStart = (Button) findViewById(R.id.btnStart);
+        buttonForResult = (Button) findViewById(R.id.btnForResult);
+
+        buttonStart.setOnClickListener(this);
+        buttonForResult.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            // 일반적인 Activity start
+            case R.id.btnStart:
+                startActivity(intentForSubActivity);
+                break;
+            // 값을 돌려받는 Activity start
+            case R.id.btnForResult:
+                startActivityForResult(intentForSubActivity, 100);
+                // startActivityForResult 두번째 인자는 호출받는 곳에서 구별할 수 있게 해주는 구분자이다.
+
+                // > start SubActivity > finish() > 결과값을 돌려준다. > MainActivity.onActivityResult(결과값) 메소드를 시스템이 호출한다.
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                                                                     // Intent에 결과값이 담겨온다.
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+}
