afe207f26c53119564c5b00704ae792b67bb96b0
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-04 00:49:48 +0900

    [MyCalculate] 계산하는 로직 추가 (정규표현식 이용)

--- Android/Mini Project/MyCalculate/app/src/main/java/com/mdy/android/mycalculate/MainActivity.java
+++ Android/Mini Project/MyCalculate/app/src/main/java/com/mdy/android/mycalculate/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     // 1. 위젯 변수를 선언
@@ -90,7 +92,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     displayView.setText(temp);
                     break;*/
                 case R.id.button_result:
-                    Toast.makeText(this, "계산이 되었습니다." + "\n\n입력한 수식은 "+displayView.getText()+" 입니다." , Toast.LENGTH_SHORT).show();
+                    String result = calculate(displayView.getText().toString());
+                    Toast.makeText(this, "입력한 수식은 "+ displayView.getText() +" 이고," + "\n\n계산한 결과는 " + result + " 입니다." , Toast.LENGTH_SHORT).show();
                     //Toast.makeText(this, ((Button) button).getText(), Toast.LENGTH_SHORT).show();  // 입력한 값을 보여준다.
                     break;
                 case R.id.button_clean:
@@ -104,6 +107,85 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void inputData(String numb){
             String temp = displayView.getText().append(numb).toString();
             displayView.setText(temp);
+        }
+
+
+        public String calculate(String preview){
+            String result = "";
+
+            // 문자열을 쪼갠 후 우선순위에 따라 연산하시오.
+            // 1. 문자열을 정규식으로 * / + - 을 이용해서 배열로 자른다
+            String splitted[] = preview.split("(?<=[*/+-])|(?=[*/+-])");
+            // 예) 123 * 45 + 67 / 89
+            // 결과값 : splitted[0] = 123
+            //          splitted[1] = *
+            //          splitted[2] = 45
+            //          splitted[3] = +
+            //          splitted[4] = 67
+            //          splitted[5] = /
+            //          splitted[6] = 89
+
+            // 배열을 중간에 삭제하기 위해서 컬렉션을 사용한다.
+            ArrayList<String> saveList = new ArrayList<>();
+
+            for(int i=0 ; i<splitted.length ; i++){
+                saveList.add(splitted[i]);
+            }
+
+            // 이렇게 해도 된다. (향상된 for문)
+                /*for(String temp : splitted){
+                    saveList.add(temp);
+                }*/
+
+
+            //        int saveListSize = saveList.size(); // 컬렉션의 크기는 밖에서 선언해주는 것을 권장한다.
+
+
+            // 반복문이 splitted 을 돌면서 * 와 / 만 먼저 연산해준다
+            for(int i=0 ; i<saveList.size(); i++ ){
+                String temp = saveList.get(i);
+                int resultTemp = 0;
+                if(temp.equals("*") || temp.equals("/")) {
+                    int before = Integer.parseInt(saveList.get(i-1));
+                    int after = Integer.parseInt(saveList.get(i+1));
+                    if(temp.equals("*"))
+                        resultTemp = before * after;
+                    else
+                        resultTemp = before / after;
+
+                    //결과값 저장
+                    //                saveList.set(i, Integer.toString(resultTemp));
+                    saveList.set(i, resultTemp+"");
+                    //필요없는 배열 뒤, 앞 2개 삭제
+                    saveList.remove(i+1);
+                    saveList.remove(i-1);
+                    i--;
+                }
+            }
+
+            // 반복문이 splitted 을 돌면서 + 와 - 만 먼저 연산해준다
+            for(int i=0 ; i<saveList.size(); i++){
+                String temp = saveList.get(i);
+                int resultTemp = 0;
+                if(temp.equals("+") || temp.equals("-")){
+                    int before = Integer.parseInt(saveList.get(i-1));
+                    int after = Integer.parseInt(saveList.get(i+1));
+                    if(temp.equals("+"))
+                        resultTemp = before + after;
+                    else
+                        resultTemp = before - after;
+
+                    // 결과값 저장
+                    //                saveList.set( i, Integer.toString(resultTemp) );
+                    saveList.set(i, resultTemp+"");
+
+                    // 필요없는 배열 뒤, 앞 2개 삭제
+                    saveList.remove(i+1);
+                    saveList.remove(i-1);
+                    i--;
+                }
+            }
+            return saveList.get(0);
         }
 }
 
