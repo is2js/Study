ff7a165cc50fe0567f7d2806c3113270df92e7c2
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-08 13:01:36 +0900

    [HandMemo] 소스코드 수정

--- Android/Mini Project/HandMemo/app/src/main/java/com/mdy/android/handmemo/MainActivity.java
+++ Android/Mini Project/HandMemo/app/src/main/java/com/mdy/android/handmemo/MainActivity.java
@@ -17,14 +17,21 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
 
     FrameLayout layout;
-    RadioGroup radioGroup;
+    RadioGroup color;
+
+//    List<Brush> brushes;
+    Path current_path;
+    Paint current_paint; // 현재 세팅된 paint
     SeekBar seekBar;
     TextView seekCount;
 
-    Paint paint = new Paint();
+    Board board;
 
 
     @Override
@@ -33,41 +40,109 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
 
         setContentView(R.layout.activity_main);
         layout = (FrameLayout) findViewById(R.id.layout);
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        color = (RadioGroup) findViewById(R.id.color);
 
-//        radioGroup.setOnCheckedChangeListener(this);
+        color.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch(checkedId) {
+                    case R.id.radioGreen :
+                        setBrush(Color.GREEN);
+                        break;
+                    case R.id.radioBlue :
+                        setBrush(Color.BLUE);
+                        break;
+                    case R.id.radioRed :
+                        setBrush(Color.RED);
+                        break;
+                }
+            }
+        });
+
 
         seekCount = (TextView) findViewById(R.id.seekCount);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
 
-//        seekBar.setOnSeekBarChangeListener(listener);
 
 
         // 1. 보드를 새로 생성한다.
-        Board board = new Board(getBaseContext());
+        board = new Board(getBaseContext());
 
-
-        radioGroup.setOnCheckedChangeListener(this);
-        seekBar.setOnSeekBarChangeListener(listener);
-
-
-        // 2. 붓을 만들어서 보드에 담는다.
-//        Paint paint = new Paint();
-//        paint.setColor(Color.MAGENTA);
-        paint.setStyle(Paint.Style.STROKE); // 이 놈의 역할은??
-//        paint.setStrokeWidth(10);
-        board.setPaint(paint);
-
-        // 3. 생성된 보드를 화면에 세팅한다.
+        // 2. 생성된 보드를 화면에 세팅한다.
         layout.addView(board);
+
+        // 3. 기본 브러쉬 세팅
+        setBrush(Color.BLACK, 10);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setBrush( , progress);
+
+
+
+                /*Brush brush = new Brush();
+                current_paint = new Paint();
+                seekCount.setText(progress+"");
+                current_paint.setStrokeWidth(progress);
+                current_paint.setStyle(Paint.Style.STROKE);
+                current_paint.setStrokeJoin(Paint.Join.ROUND);  // 연결되었을때 연결점을 둥글게 연결
+                current_paint.setStrokeCap(Paint.Cap.ROUND);  // 닫히는 부분
+                current_paint.setDither(true); // 옆으로 깨지는 것을 보정, 선을 긋게 되면 선 사이에 튀어나오는 노이즈를 제거해준다.
+                brush.paint = current_paint;
+//                brushes.add(brush);
+                board.setPaint(current_paint);
+                // 다. 완성된 브러쉬를 저장소에 담는다.*/
+//
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
     }
 
 
 
-    SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+    private void setBrush(int colorType, int thickType) {
+        // 2. 붓을 만들어서 보드에 담는다.
+        Paint paint = new Paint();
+        paint.setColor(colorType);  // ColorType으로 넘겨받는 인자를 색깔로 설정
+        paint.setStyle(Paint.Style.STROKE); // 선을 채우지 않고, 굵기를 지정
+        paint.setAntiAlias(true);   // 디자인쪽에서 나오는 용어인데
+        // 선을 만들어줄때 그 사이를 부드럽게 (경계점이 모호하게 만들어서)
+
+
+        // SeekBar에서 처리
+        paint.setStrokeJoin(Paint.Join.ROUND);  // 연결되었을때 연결점을 둥글게 연결
+        paint.setStrokeCap(Paint.Cap.ROUND);  // 닫히는 부분
+        paint.setDither(true); // 옆으로 깨지는 것을 보정, 선을 긋게 되면 선 사이에 튀어나오는 노이즈를 제거해준다.
+        paint.setStrokeWidth(thickType);
+        board.setPaint(paint);
+    }
+
+    class Brush {
+        Paint paint;
+        Path path;
+    }
+
+
+
+    /*SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             seekCount.setText(progress + "");
+            Paint paint = new Paint();
             paint.setStrokeWidth(progress);
         }
 
@@ -80,13 +155,14 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         public void onStopTrackingTouch(SeekBar seekBar) {
 
         }
-    };
+    };*/
 
-    @Override
+    /*@Override
     public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-        if(group.getId() == R.id.radioGroup) {
+        if(group.getId() == R.id.color) {
             switch (checkedId) {
                 case R.id.radioGreen :
+
                     paint.setColor(Color.GREEN);
                     Toast.makeText(this, "초록색 붓이 선택되었습니다.", Toast.LENGTH_SHORT).show();
                     break;
@@ -100,16 +176,17 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                     break;
             }
         }
-    }
+    }*/
 
 
     class Board extends View {  // 뷰를 만들었으면 onDraw() 메소드를 만들어준다.
+        List<Brush> brushes = new ArrayList<>();
         Paint paint;
-        Path path;  // 내가 터치를 하면서 움직이면 터치한 포인트들의 점을 찍은 다음에 연결해준다.
+        // Path path;  // 내가 터치를 하면서 움직이면 터치한 포인트들의 점을 찍은 다음에 연결해준다.
+
 
         public Board(Context context) {
             super(context); // super를 들어가봐서 하는 것이 많이 있으면 주석처리하면 안된다.
-            path = new Path();
         }
 
         public void setPaint(Paint paint) {
@@ -119,7 +196,9 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         @Override
         protected void onDraw(Canvas canvas) {
 //            super.onDraw(canvas);
-            canvas.drawPath(path, paint);
+            for(Brush brush : brushes) {
+                canvas.drawPath(brush.path, brush.paint);
+            }
         }
 
         @Override
@@ -133,18 +212,28 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
             switch (event.getAction()){
                 case MotionEvent.ACTION_DOWN :
                     Log.e("LOG", "onTouchEvent==================down");
-                    path = new Path();
-                    path.moveTo(x, y);  // 이전점과 현재점 사이를 그리지 않고 이동한다.
+                    // 새로운 붓을 생성 - path 와 paint를 포함한다.
+                    Brush brush = new Brush();
+                    // 가. Path 생성
+                    current_path = new Path();
+                    // 나. 생성한 패스와 현재 페인트를 브러쉬에 담는다.
+                    brush.path = current_path;
+                    brush.paint = paint;
+                    // 다. 완성된 브러쉬를 저장소에 담는다.
+                    brushes.add(brush);
+
+                    current_path.moveTo(x, y);  // 이전점과 현재점 사이를 그리지 않고 이동한다.
+
                     break;
                 case MotionEvent.ACTION_MOVE :
                     Log.e("LOG", "onTouchEvent==================move");
-                    path.lineTo(x, y);  // 바로 이전점과 현재점 사이에 줄을 그어준다.
+                    current_path.lineTo(x, y);  // 바로 이전점과 현재점 사이에 줄을 그어준다.
                     break;
                 case MotionEvent.ACTION_POINTER_UP :
                     Toast.makeText(getContext(), "언제찍히니?", Toast.LENGTH_SHORT).show();
                     break;
                 case MotionEvent.ACTION_UP :
-                    path.lineTo(x, y);
+                    current_path.lineTo(x, y);
                     break;
             }
 
