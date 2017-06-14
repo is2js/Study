632e5091f627ca1bd5a554bc51bad9f1567e7657
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-14 23:27:58 +0900

    [ServiceBasic] 소스코드 수정 및 주석내용 추가

--- Android/Mini Project/ServiceBasic/app/src/main/java/com/mdy/android/servicebasic/MainActivity.java
+++ Android/Mini Project/ServiceBasic/app/src/main/java/com/mdy/android/servicebasic/MainActivity.java
@@ -44,28 +44,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btnBind :
                 bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                // bingdService를 호출하면 onCreate가 되어있으면 바로 onBind() 메소드를 호출한다.
                 break;
             case R.id.btnUnbind :
+                unbindService(connection);
                 break;
         }
     }
 
-
-    // 액티비티가 서비스랑 통신을 할때는 connection을 통해서 하라는 뜻
+    MyService service;
+    // 액티비티가 서비스랑 통신을 해서 무엇인가를 주고 받고 싶을 때는 connection을 통해서 하라는 뜻
+    // 그래서 bindService의 두번째 인자로 connection을 넣어줬다.
+    // bindService()에 intent와 connection을 인자로 넘겨주면
+    // 액티비티가 서비스와 연결되는 순간 onServiceConnected()이 호출된다.
     ServiceConnection connection = new ServiceConnection(){
         // 서비스와 연결되는 순간 호출
+        // => onBind()가 호출되면서 리턴으로 Binder 객체를 onServiceConnected() 메소드에 넘겨준다.
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
             Log.d("MainActivity", "onServiceConnected");
 
             // binder를 통해 서비스에 접근한다.
             MyService.MyBinder iBinder = (MyService.MyBinder) binder;
-            MyService service = iBinder.getService();
+            service = iBinder.getService();   // iBinder를 통해서 서비스에 있는 기능을 꺼내서 쓸 수 있게 된다.
             service.print("연결되었습니다.");
         }
 
+        /*  정리
+        서비스와 액티비티간에 의존성이 전혀 없다.
+        근데 액티비티에서 서비스를 띄웠다는 것은 서비스에서 무엇인가 결과처리된 값을 가져오거나 서비스의 값을 조회하기 위해 많이 사용한다.
+
+        그래서 이 iBinder의 역할은 액티비티가 서비스에 의존성없이 값을 참조하기 위해서 iBinder라는 인터페이스를 사용하는 것이다.
+        그럼 이제 무엇을 할 수 있느냐면
+
+        서비스에 함수를 하나 만든다. [ex] print() 메소드
+        얘는 서비스에 있는 것이다.
+        이제 서비스의 함수를 통해서, 서비스의 리소스를 사용해서 무엇인가를 처리해줄 수 있게 된다.  */
+
+
+
+
+
+
         // 일반적인 상황에서는 호출되지 않는다. onDestory 에서는 호출되지 않는다.
-        // 서비스가 도중에 끊기거나 연결이 중단되면 호출된다.
+        // 서비스가 도중에 끊기거나 연결이 중단되면 호출된다. (네트워크가 끊긴 것처럼)
+        // 그래서 서비스를 2개 띄우면 안되니까 서비스가 살아있는지 체크하려고 onDestory에서 체크하려는 경우가 있는데
+        // onServiceDisconnected()는 그런 용도가 아니기 때문에 onDestory에서 체크할 수 없다.
         @Override
         public void onServiceDisconnected(ComponentName name) {
             Log.d("MainActivity", "onServiceDisconnected");
--- Android/Mini Project/ServiceBasic/app/src/main/java/com/mdy/android/servicebasic/MyService.java
+++ Android/Mini Project/ServiceBasic/app/src/main/java/com/mdy/android/servicebasic/MyService.java
@@ -8,10 +8,28 @@ import android.util.Log;
 
 public class MyService extends Service {
 
+    // 생성자
+    // 이 중에 통틀어 생성자함수가 가장 먼저 호출된다.
+    // 생성자가 호출되지 않으면 아래에 있는 생명주기 함수는 호출할 방법이 없다.
     public MyService() {
         Log.i("MyService", "Constructor");
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("MyService", "onCreate");
+    }
+
+
+    // MainActivity에 있는 connection이랑 MyService랑 연결해주는 연결고리의 역할을 해준다.
+    // 내가 생성한 서비스에 어떤 함수를 실행시켜서 결과값을 받는다거나 할 때,
+    // 이 onBind()를 통해서 Binder객체를 만들어서 호출한 측으로 넘겨줄 수 있다.
+    // 그래서 아래 Binder 객체를 만들었다.
+    /* 정리 */
+    // MainActivity에서 bindService()를 통해서 connection을 서비스로 넘겨주면
+    // connection안으로 onServiceConnected()의 IBinder라는 놈이 넘어오게 된다.
+    // 
     @Override
     public IBinder onBind(Intent intent) {
         Log.i("MyService", "onBind");
@@ -20,25 +38,19 @@ public class MyService extends Service {
         //throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public void print(String string) {
-        Log.i("MyService", string);
-    }
-
-    // binder의 주된 역할은 생성된
+    // 이렇게 만들어진 Binder 객체의 주된 역할은 생성된 서비스를 넘겨주는 역할을 많이 한다.
+    // Binder를 상속받아서 만든다.
+    // 이 클래스 안에 함수들을 선언하면 이 함수들을 호출한 Activity에서 직접 호출할 수 있다.??? 어디서든???
     public class MyBinder extends Binder {
         public MyService getService() {
             return MyService.this;
         }
     }
+    // Binder를 통해서 getService()를 하면 현재 생성된 서비스를 리턴해주겠다는 말이다.  ??? 현재 생성된 서비스 ???
 
 
-
-
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.i("MyService", "onCreate");
+    public void print(String string) {
+        Log.i("MyService", string);
     }
 
     @Override
