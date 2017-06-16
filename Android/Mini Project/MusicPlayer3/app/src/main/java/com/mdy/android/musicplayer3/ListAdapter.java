b0d1097ada26f46b795cb9d67b203931c9c29565
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-16 14:39:53 +0900

    [MusicPlayer3] 소스코드 수정

--- Android/Mini Project/MusicPlayer3/app/src/main/java/com/mdy/android/musicplayer3/ListAdapter.java
+++ Android/Mini Project/MusicPlayer3/app/src/main/java/com/mdy/android/musicplayer3/ListAdapter.java
@@ -151,7 +151,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             btnPause.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    switch (playerStatus){
+                    switch (Player.playerStatus){
                         case Player.PLAY:
                             Player.pause();
                             // pause 가 클릭되면 이미지 모양이 play 로 바뀐다.
