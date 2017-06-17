faf87c92974f176f32d45a9835841a09a192233d
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-17 20:44:19 +0900

    [Contacts] 주석 내용 추가

--- Android/Mini Project/Contacts/app/src/main/java/com/mdy/android/contacts/domain/Loader.java
+++ Android/Mini Project/Contacts/app/src/main/java/com/mdy/android/contacts/domain/Loader.java
@@ -15,6 +15,9 @@ import java.util.List;
 
 public class Loader {
 
+
+    // 객체지향 설계의 원칙 중에 있는 것이고,
+    // 구현체에 의존하지 않고, 부모객체나 인터페이스에 의존한다는 원칙을 따른 것이다.
     private List<Data> datas = new ArrayList<>();
     private Context context;
 
@@ -27,6 +30,7 @@ public class Loader {
 
         // 데이터베이스 혹은 content resolver를 통해 가져온 데이터를 적재할
         // 데이터 저장소를 먼저 정의한다.
+        // List<Data> datas = new ArrayList<>();    ->   위로 옮겼다.
 
 
 
@@ -42,30 +46,33 @@ public class Loader {
         Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI; // 테이블명과 같이 생각하면 된다.
         // 이너클래스
 
+
+        // URI는 하나의 테이블이라고 보면 된다. 엑셀의 시트라고 보면 된다.
         // URI 안에는 여러 종류의 데이터들이 있다.
 
         // 2. 데이터에서 가져올 컬럼명을 정의
-        String projections[] = {ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER};
+        String projections[] = {ContactsContract.CommonDataKinds.Phone.CONTACT_ID   // 고유값을 구분하기 위한 ID
+                , ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME               // 화면에 표시되는 이름
+                , ContactsContract.CommonDataKinds.Phone.NUMBER};                   // 실제 전화번호
 
         // 3. Content Resolver로 쿼리를 날려서 데이터를 가져온다.
+        // Content Resolver가 Content Provider한테 이런이런 데이터를 달라고 요청을 하는 것이다.
+        // 쿼리를 보내면 하나의 데이터묶음이 리턴이 되는데 데이터묶음의 형태가 Cursor라는 형태의 객체로 되어 있다. (ArrayList와 비슷한 형태, 데이터가 목록형태로 데이터가 들어가 있다.)
         // 결과값을 Cursor의 형태로 반환한다. ArrayList와 같은 형태
-        Cursor cursor = resolver.query(phoneUri,    // 데이터의 주소
-                projections,    // 가져올 데이터 컬럼명 배열
+        Cursor cursor = resolver.query(phoneUri,    // 데이터의 주소 (URI)
+                projections,    // 가져올 데이터 컬럼명 배열 (projection)
                 null,           // 조건절에 들어가는 컬럼명들 지정
                 null,           // 지정된 컬럼명과 매핑되는 실제 조건 값
                 null);          // 정렬
 
 
-        // cursor에 데이터 존재여부
         // 4. 반복문을 통해 cursor에 담겨있는 데이터를 하나씩 추출한다.
-        if (cursor != null) {
+        if (cursor != null) {   // cursor에 데이터 존재여부 체크
             while (cursor.moveToNext()) {
                 // moveToNext() 다음 데이터로 이동, 데이터가 있으면 계속 while문이 동작
                 // moveToNext()가 true, false를 반환하기 때문에 while문으로 하는 것이 적합
                 // 4.1 위에 정의한 프로젝션의 컬럼명으로 cursor 있는 인덱스값을 조회하고
-                int idIndex = cursor.getColumnIndex(projections[0]);
+                int idIndex = cursor.getColumnIndex(projections[0]);    // projection에서 어떤 컬럼을 가져올지를 Index로 지정
                 int id = cursor.getInt(idIndex);
                 // int id = cursor.getInt(0);   위의 2줄을 이렇게 해줘도 된다.
                 // 그런데 코드의 가독성을 위해 위와 같이 한다.
