2d924b4cbcad80d0d4fed6c5d952b2e7abf6da05
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-26 22:18:07 +0900

    [HttpBbs3] 프로젝트 생성 및 소스코드 작성

---
+++ Android/Mini Project/HttpBbs3/app/src/main/java/com/mdy/android/httpbbs3/CustomAdapter.java
@@ -0,0 +1,67 @@
+package com.mdy.android.httpbbs3;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by MDY on 2017-06-26.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.Holder>{
+
+    List<Bbs> list;
+
+    public CustomAdapter (){
+        list = new ArrayList<>();
+    }
+
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        Bbs bbs = list.get(position);
+
+        holder.txtId.setText(bbs.id+"");
+        holder.txtTitle.setText(bbs.title);
+        holder.txtContent.setText(bbs.content);
+        holder.txtAuthor.setText(bbs.author);
+        holder.txtDate.setText(bbs.date);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public void setList(List<Bbs> list){
+        this.list = list;
+    }
+
+
+
+    class Holder extends RecyclerView.ViewHolder{
+        TextView txtId, txtTitle, txtContent, txtAuthor, txtDate;
+
+        public Holder(View itemView) {
+            super(itemView);
+            txtId = (TextView) itemView.findViewById(R.id.txtId);
+            txtTitle = (TextView) itemView.findViewById(R.id.txtTitle);
+            txtContent = (TextView) itemView.findViewById(R.id.txtContent);
+            txtAuthor = (TextView) itemView.findViewById(R.id.txtAuthor);
+            txtDate = (TextView) itemView.findViewById(R.id.txtDate);
+        }
+    }
+}
---
+++ Android/Mini Project/HttpBbs3/app/src/main/java/com/mdy/android/httpbbs3/Data.java
@@ -0,0 +1,11 @@
+package com.mdy.android.httpbbs3;
+
+import java.util.List;
+
+/**
+ * Created by MDY on 2017-06-26.
+ */
+
+public class Data {
+    List<Bbs> bbsList;
+}
