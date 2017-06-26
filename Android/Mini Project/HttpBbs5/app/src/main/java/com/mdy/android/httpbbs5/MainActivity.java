3e550c25c09d197e0353d1af78f469541e8dff8f
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-26 23:21:47 +0900

    [HttpBbs5] 소스코드 작성

--- Android/Mini Project/HttpBbs5/app/src/main/java/com/mdy/android/httpbbs5/MainActivity.java
+++ Android/Mini Project/HttpBbs5/app/src/main/java/com/mdy/android/httpbbs5/MainActivity.java
@@ -1,13 +1,58 @@
 package com.mdy.android.httpbbs5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DataLoader.CallBack{
+
+    TextView txtId, txtTitle, txtContent, txtAuthor, txtDate;
+    RecyclerView recycler;
+
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setViews();
+
+        // 1. 데이터 정의
+        DataLoader loader = new DataLoader();
+        loader.getData("http://192.168.10.79:8080/bbs/json/list", this);
+
+        // 2. 아답터 생성
+        adapter = new CustomAdapter();
+
+        // 3. 연결
+        recycler.setAdapter(adapter);
+
+        // 4. 레이아웃 매니저 등록
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+
+
+
+    public void setViews(){
+        txtId = (TextView) findViewById(R.id.txtId);
+        txtTitle = (TextView) findViewById(R.id.txtTitle);
+        txtContent = (TextView) findViewById(R.id.txtContent);
+        txtAuthor = (TextView) findViewById(R.id.txtAuthor);
+        txtDate = (TextView) findViewById(R.id.txtDate);
+
+        recycler = (RecyclerView) findViewById(R.id.recycler);
+    }
+
+    @Override
+    public void setData(List<Bbs> list) {
+        adapter.setList(list);
+        adapter.notifyDataSetChanged();
     }
 }
