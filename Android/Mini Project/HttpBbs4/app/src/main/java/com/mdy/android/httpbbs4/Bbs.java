c7d0147682b2508fff60c1ac8639fc9465224ea0
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-06-26 22:18:53 +0900

    [HttpBbs4] 프로젝트 생성 후 코드 작성

---
+++ Android/Mini Project/HttpBbs4/app/src/main/java/com/mdy/android/httpbbs4/Bbs.java
@@ -0,0 +1,13 @@
+package com.mdy.android.httpbbs4;
+
+/**
+ * Created by MDY on 2017-06-26.
+ */
+
+public class Bbs {
+    int id;
+    String title;
+    String content;
+    String author;
+    String date;
+}
