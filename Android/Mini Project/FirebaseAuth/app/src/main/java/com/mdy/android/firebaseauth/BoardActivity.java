492f7770bba857feca00dfffef4dee4fa6d74bd7
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-09 02:55:31 +0900

    [FirebaseAuth] 데이터베이스 Transaction 코드 작성

--- Android/Mini Project/FirebaseAuth/app/src/main/java/com/mdy/android/firebaseauth/BoardActivity.java
+++ Android/Mini Project/FirebaseAuth/app/src/main/java/com/mdy/android/firebaseauth/BoardActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -26,12 +31,14 @@ public class BoardActivity extends AppCompatActivity {
     private List<ImageDTO> imageDTOs = new ArrayList<>();
     private List<String> uidLists = new ArrayList<>();
     private FirebaseDatabase database;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
         database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -42,9 +49,12 @@ public class BoardActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 imageDTOs.clear();
+                uidLists.clear();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     ImageDTO imageDTO = snapshot.getValue(ImageDTO.class);
+                    String uidKey = snapshot.getKey();
                     imageDTOs.add(imageDTO);
+                    uidLists.add(uidKey);
                 }
                 adapter.notifyDataSetChanged();
 
@@ -67,16 +77,28 @@ public class BoardActivity extends AppCompatActivity {
 
         @Override
         public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_board, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_board, parent, false);
             return new Holder(view);
         }
 
         @Override
-        public void onBindViewHolder(Holder holder, int position) {
+        public void onBindViewHolder(Holder holder, final int position) {
             holder.txtTitle.setText(imageDTOs.get(position).title);
             holder.txtDescription.setText(imageDTOs.get(position).description);
 
             Glide.with(holder.imageView.getContext()).load(imageDTOs.get(position).imageUrl).into(holder.imageView);
+            holder.imageViewStar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onStarClicked(database.getReference().child("images").child(uidLists.get(position)));
+                }
+            });
+
+            if (imageDTOs.get(position).stars.containsKey(auth.getCurrentUser().getUid())) {
+                holder.imageViewStar.setImageResource(R.drawable.black_heart);
+            } else {
+                holder.imageViewStar.setImageResource(R.drawable.black_heart_outline);
+            }
         }
 
         @Override
@@ -84,14 +106,49 @@ public class BoardActivity extends AppCompatActivity {
             return imageDTOs.size();
         }
 
+        private void onStarClicked(DatabaseReference postRef) {
+            postRef.runTransaction(new Transaction.Handler() {
+                @Override
+                public Transaction.Result doTransaction(MutableData mutableData) {
+                    ImageDTO p = mutableData.getValue(ImageDTO.class);
+                    if (p == null) {
+                        return Transaction.success(mutableData);
+                    }
+
+                    if (p.stars.containsKey(auth.getCurrentUser().getUid())) {
+                        // Unstar the post and remove self from stars
+                        p.starCount = p.starCount - 1;
+                        p.stars.remove(auth.getCurrentUser().getUid());
+                    } else {
+                        // Star the post and add self to stars
+                        p.starCount = p.starCount + 1;
+                        p.stars.put(auth.getCurrentUser().getUid(), true);
+                    }
+
+                    // Set value and report transaction success
+                    mutableData.setValue(p);
+                    return Transaction.success(mutableData);
+                }
+
+                @Override
+                public void onComplete(DatabaseError databaseError, boolean b,
+                                       DataSnapshot dataSnapshot) {
+                    // Transaction completed
+                    Log.d("onComplete TAG", "postTransaction:onComplete:" + databaseError);
+                }
+            });
+        }
+
         class Holder extends RecyclerView.ViewHolder{
             ImageView imageView;
             TextView txtTitle, txtDescription;
+            ImageView imageViewStar;
             public Holder(View itemView) {
                 super(itemView);
                 imageView = (ImageView) itemView.findViewById(R.id.imageView);
                 txtTitle = (TextView) itemView.findViewById(R.id.txtTitle);
                 txtDescription = (TextView) itemView.findViewById(R.id.txtDescription);
+                imageViewStar = (ImageView) itemView.findViewById(R.id.imageViewStar);
             }
         }
     }
--- Android/Mini Project/FirebaseAuth/app/src/main/java/com/mdy/android/firebaseauth/ImageDTO.java
+++ Android/Mini Project/FirebaseAuth/app/src/main/java/com/mdy/android/firebaseauth/ImageDTO.java
@@ -1,5 +1,8 @@
 package com.mdy.android.firebaseauth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by MDY on 2017-07-08.
  */
@@ -11,5 +14,8 @@ public class ImageDTO {
     public String description;
     public String uid;
     public String userId;
+
+    public int starCount = 0;
+    public Map<String, Boolean> stars = new HashMap<>();
 
 }
