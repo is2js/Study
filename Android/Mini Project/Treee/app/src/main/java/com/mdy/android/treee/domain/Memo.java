e78ad73980d2db020fc797610ffaf540a23efd2f
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-11 02:10:06 +0900

    [Treee] domain 구성 (Memo, Data)

---
+++ Android/Mini Project/Treee/app/src/main/java/com/mdy/android/treee/domain/Memo.java
@@ -0,0 +1,24 @@
+package com.mdy.android.treee.domain;
+
+/**
+ * Created by MDY on 2017-07-11.
+ */
+
+public class Memo {
+    public String content1;
+    public String content2;
+    public String content3;
+
+    public String fileUriString;
+    public String date;
+
+    public Memo(){
+
+    }
+
+    public Memo(String content1, String content2, String content3){
+        this.content1 = content1;
+        this.content2 = content2;
+        this.content3 = content3;
+    }
+}
