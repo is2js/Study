a40df488a2214f354f9c4241946ccb8eb2f77a2b
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-11 02:09:43 +0900

    [Treee] FeedActivity 뷰 작성, FeedAdapter 코드 작성

--- Android/Mini Project/Treee/app/src/main/java/com/mdy/android/treee/FeedActivity.java
+++ Android/Mini Project/Treee/app/src/main/java/com/mdy/android/treee/FeedActivity.java
@@ -1,13 +1,33 @@
 package com.mdy.android.treee;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class FeedActivity extends AppCompatActivity {
+
+    ImageView btnMinus, btnFeedIcon, btnProfile;
+    ImageView imageViewTopTree, imageViewBottomTree;
+    ImageView imageViewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        setViews();
+
+
+    }
+
+    public void setViews(){
+        btnMinus = (ImageView) findViewById(R.id.btnMinus);
+        btnFeedIcon = (ImageView) findViewById(R.id.btnFeedIcon);
+        btnProfile = (ImageView) findViewById(R.id.btnProfile);
+
+        imageViewTopTree = (ImageView) findViewById(R.id.imageViewTopTree);
+        imageViewBottomTree = (ImageView) findViewById(R.id.imageViewBottomTree);
+
+        imageViewText = (ImageView) findViewById(R.id.imageViewText);
+
     }
 }
