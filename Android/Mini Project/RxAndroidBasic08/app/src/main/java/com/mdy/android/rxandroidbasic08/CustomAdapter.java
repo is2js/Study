ef345912806afc4df4dc9c6d70cfddd8b7af2f6d
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-23 22:31:38 +0900

    [RxAndroidBasic08] 소스코드 수정

--- Android/Mini Project/RxAndroidBasic08/app/src/main/java/com/mdy/android/rxandroidbasic08/CustomAdapter.java
+++ Android/Mini Project/RxAndroidBasic08/app/src/main/java/com/mdy/android/rxandroidbasic08/CustomAdapter.java
@@ -7,8 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.mdy.android.rxandroidbasic08.domain.Info;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,12 +16,15 @@ import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.Holder>{
 
-    List<Info> data = null;
+    List<String> rows = new ArrayList<>();
     LayoutInflater inflater = null;
 
-    public CustomAdapter(Context context, List<Info> data){
+    public CustomAdapter(Context context){
         this.inflater = LayoutInflater.from(context);
-        this.data = data;
+    }
+
+    public void setData(List<String> rows){
+        this.rows = rows;
     }
 
     @Override
@@ -33,34 +35,20 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.Holder>{
 
     @Override
     public void onBindViewHolder(Holder holder, int position) {
-        Info info = data.get(position);
-        holder.txtName.setText(info.name);
-        holder.txtTemperature.setText(info.temperature+"");
-        holder.txtHumidity.setText(info.humidity+"");
-        holder.txtRain.setText(info.rain+"");
-
-
-//        holder.txtName.setText(rows.);
-
+        holder.textView.setText(rows.get(position).toString());
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return rows.size();
     }
 
     class Holder extends RecyclerView.ViewHolder {
-        TextView txtName;
-        TextView txtTemperature;
-        TextView txtHumidity;
-        TextView txtRain;
+        TextView textView;
 
         public Holder(View itemView) {
             super(itemView);
-            txtName = (TextView) itemView.findViewById(R.id.txtName);
-            txtTemperature = (TextView) itemView.findViewById(R.id.txtTemperature);
-            txtHumidity = (TextView) itemView.findViewById(R.id.txtHumidity);
-            txtRain = (TextView) itemView.findViewById(R.id.txtRain);
+            textView = (TextView) itemView.findViewById(R.id.textView);
         }
     }
 }
--- Android/Mini Project/RxAndroidBasic08/app/src/main/java/com/mdy/android/rxandroidbasic08/MainActivity.java
+++ Android/Mini Project/RxAndroidBasic08/app/src/main/java/com/mdy/android/rxandroidbasic08/MainActivity.java
@@ -7,9 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mdy.android.rxandroidbasic08.domain.Data;
-import com.mdy.android.rxandroidbasic08.domain.Info;
 import com.mdy.android.rxandroidbasic08.domain.Row;
 
 import java.util.ArrayList;
@@ -35,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSearch;
     private RecyclerView recycler;
 
-    // 데이터 정의
-    List<Info> data = new ArrayList<>();
     private CustomAdapter adapter;
 
     @Override
@@ -44,49 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setViews();
-
-        /*
-
-        // 1. Retrofit 생성
-        Retrofit client = new Retrofit.Builder()
-                .baseUrl(SERVER)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-
-        // 2. 서비스 생성
-        IWeather service = client.create(IWeather.class);
-
-        // 3. 옵저버블 생성 ( addCallAdapterFactory(RxJava2CallAdapterFactory.create()) 이걸 해줬기 때문에 가능 )
-        Observable<Data> observable = service.getData(SERVER_KEY, 1, 10, "서초");
-
-        // 4. 발행 시작
-        observable.subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.newThread())
-                // 구독 시작
-                .subscribe(
-                        data -> {
-                            Row rows[] = data.getRealtimeWeatherStation().getRow();
-                            for (Row row : rows) {
-                                Log.i("Weather", "지역명 = " + row.getSTN_NM());
-                                Log.i("Weather", "기온 = " + row.getSAWS_TA_AVG() + "도");
-                                Log.i("Weather", "습도 = " + row.getSAWS_HD() + "%");
-                                Log.i("Weather", "일조 = " + row.getSAWS_SHINE() + "hour");
-                            }
-
-                        }
-                );
-
-        */
-
-
-
-
-
-
-
-
-
+        setListeners();
     }
 
     private void setViews() {
@@ -94,55 +50,65 @@ public class MainActivity extends AppCompatActivity {
         btnSearch = (Button) findViewById(R.id.btnSearch);
         recycler = (RecyclerView) findViewById(R.id.recycler);
 
-        adapter = new CustomAdapter(this, data);
+        adapter = new CustomAdapter(this);
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    // doSearch
-    public void doSearch(View view){
-        String gu = editTextName.getText().toString();
+    // Search
+    public void setListeners() {
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 1. Retrofit 생성
+                Retrofit client = new Retrofit.Builder()
+                        .baseUrl(SERVER)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .build();
 
-        // 1. Retrofit 생성
-        Retrofit client = new Retrofit.Builder()
-                .baseUrl(SERVER)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
+                // 2. 서비스 생성
+                IWeather service = client.create(IWeather.class);
 
-        // 2. 서비스 생성
-        IWeather service = client.create(IWeather.class);
+                // 3. 옵저버블 생성 ( addCallAdapterFactory(RxJava2CallAdapterFactory.create()) 이걸 해줬기 때문에 가능 )
+//                Observable<Data> observable = service.getData(SERVER_KEY, 1, 10, editTextName.getText().toString() + "");
+                Observable<Data> observable = service.getData(SERVER_KEY, 1, 10, "서초");
 
-        // 3. 옵저버블 생성 ( addCallAdapterFactory(RxJava2CallAdapterFactory.create()) 이걸 해줬기 때문에 가능 )
-        Observable<Data> observable = service.getData(SERVER_KEY, 1, 10, gu);
+                // 4. 발행 시작
+                observable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        // 구독 시작
+                        .subscribe(
+                                item -> {
+                                    // 예외 처리
+                                    if (item.getRealtimeWeatherStation() == null) {
+                                        Toast.makeText(getApplicationContext(), "데이터 형식이 올바르지 않습니다.", Toast.LENGTH_LONG).show();
+                                        return;     // return; 을 해줘야 null 일때 아래 코드가 실행되지 않는다.
+                                    }
 
-        // 4. 발행 시작
-        observable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                // 구독 시작
-                .subscribe(
-                        item -> {
-                            Row rows[] = item.getRealtimeWeatherStation().getRow();
-                            for(Row row : rows){
-                                Info info = new Info();
-                                info.name = row.getNAME();
-                                info.temperature = row.getSAWS_TA_AVG();
-                                info.humidity = row.getSAWS_HD();
-                                info.rain = row.getSAWS_RN_SUM();
+                                    Toast.makeText(MainActivity.this, item.getRealtimeWeatherStation().getRESULT().getMESSAGE(), Toast.LENGTH_SHORT).show();
+                                    List<String> content = new ArrayList<>();
+                                    Row rows[] = item.getRealtimeWeatherStation().getRow();
 
-                                data.add(info);
-                            }
-                            adapter.notifyDataSetChanged();
-
-
-//                                Log.i("Weather", "지역명 = " + row.getSTN_NM());
-//                                Log.i("Weather", "기온 = " + row.getSAWS_TA_AVG() + "도");
-//                                Log.i("Weather", "습도 = " + row.getSAWS_HD() + "%");
-//                                Log.i("Weather", "일조 = " + row.getSAWS_SHINE() + "hour");
-
-
-                            }
-                );
+                                    for (Row row : rows) {
+                                        content.add(row.getSAWS_OBS_TM());  // 측정일시
+                                        content.add(row.getSTN_NM());       // 지점명
+                                        content.add(row.getSTN_ID());       // 지점코드
+                                        content.add(row.getSAWS_TA_AVG());  // 기온
+                                        content.add(row.getSAWS_HD());      // 습도
+                                        content.add(row.getCODE());         // 풍향1
+                                        content.add(row.getNAME());         // 풍향2
+                                        content.add(row.getSAWS_WS_AVG());  // 풍속
+                                        content.add(row.getSAWS_RN_SUM());  // 강수
+                                        content.add(row.getSAWS_SOLAR());   // 일사
+                                        content.add(row.getSAWS_SHINE());   // 일조
+                                    }
+                                    adapter.setData(content);
+                                    adapter.notifyDataSetChanged();
+                                }
+                        );
+            }
+        });
     }
 }
 
@@ -154,37 +120,3 @@ interface IWeather {
             , @Path("count") int offset
             , @Path("name") String gu);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
