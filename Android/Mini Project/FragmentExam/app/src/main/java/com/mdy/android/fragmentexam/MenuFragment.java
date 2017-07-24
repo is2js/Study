fff2199129c222994cf183dcaf8296bf55beb9b3
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-24 23:01:39 +0900

    [FragmentExam] 프래그먼트간 화면전환 코드 작성

--- Android/Mini Project/FragmentExam/app/src/main/java/com/mdy/android/fragmentexam/MenuFragment.java
+++ Android/Mini Project/FragmentExam/app/src/main/java/com/mdy/android/fragmentexam/MenuFragment.java
@@ -12,18 +12,18 @@ import android.widget.Button;
  * Created by MDY on 2017-07-24.
  */
 
-public class MainFragment extends Fragment {
+public class MenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
 
         Button button = (Button) rootView.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity activity = (MainActivity) getActivity();
-                activity.onFragmentChanged(0);
+                activity.onFragmentChanged(1);
             }
         });
 
