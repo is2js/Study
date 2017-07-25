0deda7371db7825ca788858b06eff429b093de43
DESKTOP-NLSVA5H\MDY <dongyeon89@gmail.com>
2017-07-25 11:52:33 +0900

    [ServerNodejs] retrofit interface 추가

--- Android/Mini Project/ServerNodejs/app/src/main/java/android/mdy/com/servernodejs/MainActivity.java
+++ Android/Mini Project/ServerNodejs/app/src/main/java/android/mdy/com/servernodejs/MainActivity.java
@@ -2,31 +2,59 @@ package android.mdy.com.servernodejs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnWrite;
     private RecyclerView recyclerView;
+    private List<Bbs> data;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lambdaTest();
-        initView();
+        setViews();
+        data = new ArrayList<>();
+        adapter = new RecyclerAdapter(this, data);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void lambdaTest() {
-        new Thread(
-                () -> Log.i("Lambda", "running============ok")
-        ).start();
-    }
 
-    private void initView() {
+    private void setViews() {
         btnWrite = (Button) findViewById(R.id.btnWrite);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    private void loader() {
+
+    }
+
+    interface myServer {
+        public static final String SERVER = "http://192.168.10.79/";
+        @GET("bbs")
+        public Observable<Bbs> read();
+
+        @POST("bbs")
+        public void write(Bbs bbs);
+
+        @PUT("bbs")
+        public void update(Bbs bbs);
+
+        @DELETE("bbs")
+        public void delete(Bbs bbs);
     }
 }
--- Android/Mini Project/ServerNodejs/app/src/main/java/android/mdy/com/servernodejs/RecyclerAdapter.java
+++ Android/Mini Project/ServerNodejs/app/src/main/java/android/mdy/com/servernodejs/RecyclerAdapter.java
@@ -1,33 +1,47 @@
 package android.mdy.com.servernodejs;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by MDY on 2017-07-25.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Holder> {
+    LayoutInflater inflater;
+    List<Bbs> data;
 
-    public RecyclerAdapter(){
-
+    public RecyclerAdapter(Context context, List<Bbs> data){
+        this.data = data;
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_list, parent, false);
+        return new Holder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(Holder holder, int position) {
+        Bbs bbs = data.get(position);
+        holder.setTitle(bbs.title);
+        holder.setContent(bbs.content);
+        holder.setAuthor(bbs.author);
+        holder.setDate(bbs.date);
     }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     class Holder extends RecyclerView.ViewHolder{
