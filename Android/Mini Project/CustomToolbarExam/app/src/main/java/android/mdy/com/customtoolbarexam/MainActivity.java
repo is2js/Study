3f2369fdf36e2275aa5a2c5030dad7ffc1e1db1c
Daniel Min <dongyeon89@gmail.com>
2017-08-31 18:02:58 +0900

    [CustomToolbarExam] Custom Actionbar 코드 작성

--- Android/Mini Project/CustomToolbarExam/app/src/main/java/android/mdy/com/customtoolbarexam/MainActivity.java
+++ Android/Mini Project/CustomToolbarExam/app/src/main/java/android/mdy/com/customtoolbarexam/MainActivity.java
@@ -1,13 +1,79 @@
 package android.mdy.com.customtoolbarexam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton btnAlarm, btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    private void setViews(){
+        btnAlarm = (ImageButton) findViewById(R.id.btnAlarm);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+    }
+
+    private void setListeners(){
+        btnAlarm.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnAlarm:
+                Toast.makeText(this, "알람 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnBack:
+                Toast.makeText(this, "Back 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        ActionBar actionBar = getSupportActionBar();
+
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+
+
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
+
+        actionBar.setCustomView(actionbar);
+
+        //액션바 양쪽 공백 없애기
+        Toolbar parent = (Toolbar)actionbar.getParent();
+        parent.setContentInsetsAbsolute(0,0);
+
+        setViews();
+        setListeners();
+
+        return true;
+    }
+
+
 }
